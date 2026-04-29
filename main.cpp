46f4fe398349ede0fe3a699fc1763011865c9a16
Christine Susic <csusic022@insite.4cd.edu>
2026-04-29 14:27:53 -0700

    Commit

---
+++ main.cpp
@@ -0,0 +1,86 @@
+// COMSC-210 | Lab 34 | Christine Susic
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+const int SIZE = 7;
+
+struct Edge {
+    int src, dest, weight;
+};
+
+typedef pair<int, int> Pair;  // Creates alias 'Pair' for the pair<int,int> data type
+
+class Graph {
+public:
+    // a vector of vectors of Pairs to represent an adjacency list
+    vector<vector<Pair>> adjList;
+
+    // Graph Constructor
+    Graph(vector<Edge> const &edges) {
+        // resize the vector to hold SIZE elements of type vector<Edge>
+        adjList.resize(SIZE);
+
+        // add edges to the directed graph
+        for (auto &edge: edges) {
+            int src = edge.src;
+            int dest = edge.dest;
+            int weight = edge.weight;
+
+            // insert at the end
+            adjList[src].push_back(make_pair(dest, weight));
+            // for an undirected graph, add an edge from dest to src also
+            adjList[dest].push_back(make_pair(src, weight));
+        }
+    }
+    
+    //depth first search
+    void DFS(int v, vector<bool> &visited) {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        cout << v << " ";
+
+        // Recur for all the vertices adjacent to this vertex
+        for (Pair neighbor : adjList[v]) {
+            int u = neighbor.first;
+            if (!visited[u]) {
+                DFS(u, visited);
+            }
+        }
+    }
+
+    // Print the graph's adjacency list
+    void printGraph() {
+        cout << "Graph's adjacency list:" << endl;
+        for (int i = 0; i < adjList.size(); i++) {
+            cout << i << " --> ";
+            for (Pair v : adjList[i])
+                cout << "(" << v.first << ", " << v.second << ") ";
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    // Creates a vector of graph edges/weights
+    vector<Edge> edges = {
+        // (x, y, w) —> edge from x to y having weight w
+        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+    };
+
+    // Creates graph
+    Graph graph(edges);
+
+    // Prints adjacency list representation of graph
+    graph.printGraph();
+    
+    cout << "DFS starting from vertex 0: " << endl;
+    vector<bool> visited(SIZE, false);
+    graph.DFS(0, visited);
+    cout << endl;
+    cout << "BFS starting from vertex 0: " << endl;
+
+    return 0;
+}
